ac69cea2e388924206772754085481b22e35a0f3
mads <mads@sina.com>
2014-10-10 13:22:19 +0800

    di yi ci genggai

---
+++ test3/firstViewController.m
@@ -0,0 +1,50 @@
+//
+//  firstViewController.m
+//  test3
+//
+//  Created by promo on 14-10-10.
+//  Copyright (c) 2014年 promo. All rights reserved.
+//
+
+#import "firstViewController.h"
+
+@interface firstViewController ()
+
+@end
+
+@implementation firstViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    UIButton * btn = [UIButton buttonWithType:UIButtonTypeInfoLight];
+    btn.frame = CGRectMake(40, 50, 100, 30);
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
